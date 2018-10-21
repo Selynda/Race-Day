91b42556870aed5f4c2fa957d265ba455d1a0231
Selynda <42097718+Selynda@users.noreply.github.com>
2018-10-21 13:31:50 -0500

    next step

--- src/southlandProgarm_stage2.py
+++ src/southlandProgarm_stage2.py
@@ -4,16 +4,32 @@
 postColors = []
 postColors = ["Red", "Blue", "Silver", "Green", "Black", "Yellow", "Orange", "Ylk/Blk"]
 
-# working variables
+# Race date patterns to look for
 
-assembleLine = ""
+raceDate = []
+raceDate = ["01/","02/","03/","04/","05/","06/","07/","08/","09/","10/","11/","12/"]
 
 # File being worked on
 
-workFile = open("C:/Users/sgnol/Documents/projects/race_day/data/southland.txt, "r")
+inFile = open("C:/Users/sgnol/Documents/projects/race_day/data/southland_pdf.txt", "r")
 
 # Output file with final product
 
-outFile = open('C:/Users/sgnol/Documents/projects/race_day/data/southland.txt', 'a+')
+outFile = open("C:/Users/sgnol/Documents/projects/race_day/data/southland.txt", 'a+')
 outFile.seek(0)
 outFile.truncate()
+
+# working variables
+
+assembleLine = ""
+isEndOfFile = False
+color = ""
+
+
+while isEndOfFile == False:
+
+    fileLine = inFile.readline()
+
+    if fileLine[0:15] in postColors:
+        print (fileLine[0:15])
+        isEndOfFile = True
