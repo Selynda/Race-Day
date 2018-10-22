2132c3c8fa4637f687148beccc55e2d64d3c31a3
Selynda <42097718+Selynda@users.noreply.github.com>
2018-10-21 22:22:19 -0500

    stage 2 code dev

--- src/southlandProgram_stage2.py
+++ src/southlandProgram_stage2.py
@@ -1,8 +1,12 @@
 
 # List of post colors
 
-postColors = []
-postColors = ["Red", "Blue", "Silver", "Green", "Black", "Yellow", "Orange", "Ylk/Blk"]
+# postColors = []
+# postColors = ["Red", "Blue", "Silver", "Green", "Black", "Yellow", "Orange", "Ylk/Blk"]
+
+
+postColors = {}
+postColors = {"Red", "Blue", "Silver", "Green", "Black", "Yellow", "Orange", "Ylk/Blk"}
 
 # Race date patterns to look for
 
@@ -21,17 +25,61 @@ outFile.truncate()
 
 # working variables
 
+linePrefix = ""
 assembleLine = ""
 isEndOfFile = False
 color = ""
+x = 0
 
 
 while isEndOfFile == False:
 
     fileLine = inFile.readline()
+    if fileLine == "":
+        isEndOfFile = True
+        break
 
-    if fileLine[0:15] in postColors:
-        print (fileLine[0:15])
+    linePrefix = ""
+
+    searchLine = fileLine[0:15]
+
+    color = postColors.intersection(set(searchLine))
+    print (searchLine + " " + str(postColors.intersection(set(searchLine))))
+
+   
+    # if searchLine.find("Red") >= 0:
+    #     linePrefix = fileLine[0:(fileLine.find("Red")+3)] + "\t"
+
+    # if searchLine.find("Blue") >= 0:
+    #     linePrefix = fileLine[0:(fileLine.find("Blue")+4)] + "\t"
+
+    # if searchLine.find("Silver") >= 0:
+    #     linePrefix = fileLine[0:(fileLine.find("Silver")+6)] + "\t"
+
+    # if searchLine.find("Green") >= 0:
+    #     linePrefix = fileLine[0:(fileLine.find("Green")+5)] + "\t"
+
+    # if searchLine.find("Black") >= 0:
+    #     linePrefix = fileLine[0:(fileLine.find("Black")+5)] + "\t"
+
+    # if searchLine.find("Yellow") >= 0:
+    #     linePrefix = fileLine[0:(fileLine.find("Yellow")+6)] + "\t"
+    
+    # if searchLine.find("Orange") >= 0:
+    #     linePrefix = fileLine[0:(fileLine.find("Orange")+6)] + "\t"
+    
+    # if searchLine.find("Ylw/Blk") >= 0:
+    #     linePrefix = fileLine[0:(fileLine.find("Ylw/Blk")+7)] + "\t"       
+
+# Search for date pattern
+
+    
 
 
-    isEndOfFile = True
+
+
+
+    # x += 1
+    # if x >= 8:
+    #     isEndOfFile = True
+    #     break
